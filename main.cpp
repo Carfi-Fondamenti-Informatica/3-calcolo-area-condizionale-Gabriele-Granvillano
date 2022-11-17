eace4db1824653b29817de6adbf197efec07f9ff
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-11-17 19:17:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    float a,b,area;
+    int opzione;
+    cin >> a >> b >> opzione;
+    switch (opzione)
+    {
+        case 0:
+            area = a*b/2;
+            cout << area << endl;
+            break;
+        case 1:
+            area = a*a;
+            cout << area << endl;
+            break;
+        case 2:
+            area = a*b;
+            cout << area << endl;
+            break;
+        default:
+            cout << "opzione non valida" << endl;
+    }
+    return 0;
 }
